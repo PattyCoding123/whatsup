d263b8d26e5c35e32c49436a1724060aaf42a93f
PattyCoding123 <patrickducusin2@gmail.com>
2022-08-23 03:34:40 -0400

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_FIREBASE_SENDER_ID,
-  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
